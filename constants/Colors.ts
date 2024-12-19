ce60c951fa2cd2e2210a915612dc17b11ee0f6bb
ruben <ruben.dunesme@etudiant.univ-reims.fr>
2024-12-19 13:48:59 +0100

    feat: Adding project colors

---
+++ constants/Colors.ts
@@ -0,0 +1,15 @@
+export  const Colors = {
+    light: {
+        primary: '#E4FAFF',
+        modify: '#CC7700',
+        remove: '#E90000',
+        background: '#1C1C1C',
+    },
+    dark: {
+        primary: '#E4FAFF',
+        modify: '#CC7700',
+        remove: '#E90000',
+        background: '#1C1C1C',
+    },
+
+}
