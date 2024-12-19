3d96195683fdd65278ccc9f66ae894787c8fa574
ruben <ruben.dunesme@etudiant.univ-reims.fr>
2024-12-19 19:39:20 +0100

    feat: PostType & PostButton component

---
+++ components/Post.tsx
@@ -0,0 +1,13 @@
+import { Post } from "@/constants/PostType";
+import { Card } from "./Card";
+
+type Props = Post
+
+export function Post({data}: Props){
+
+    return(
+        <Card></Card>
+    )
+
+
+}
---
+++ constants/PostType.ts
@@ -0,0 +1,6 @@
+export type Post = {
+    userId: number
+    text?: string,
+    imgUri?: string,
+    date: string
+  }
