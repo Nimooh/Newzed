74a24347805477cc9a23befae404ae77d9693370
ruben <ruben.dunesme@etudiant.univ-reims.fr>
2024-12-19 19:40:12 +0100

    feat: adding Navbar to index

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,32 @@
-import { Text, View } from "react-native";
+import { Navbar } from "@/components/Navbar";
+import { PostForm } from "@/components/PostForm";
+import { useThemeColors } from "@/hooks/useThemeColors";
+import { useState } from "react";
+import { KeyboardAvoidingView, Platform, ScrollView, StyleSheet, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
+  const colors = useThemeColors()
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      style={{ flex: 1 }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+        <SafeAreaView style={[styles.root,{backgroundColor: colors.background}]}>
+          <ScrollView contentContainerStyle={{ flexGrow: 1 }} style={styles.body}></ScrollView>
+          <Navbar/>          
+        </SafeAreaView>
+      </KeyboardAvoidingView>
+
   );
 }
+
+const styles = StyleSheet.create({
+  root: {
+    flex:1,
+  },
+  body: {
+    flex:1,
+
+  }
+})
