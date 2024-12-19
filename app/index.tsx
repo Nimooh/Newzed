97a470857ca714d0eb971bc26cd0240ca2866859
ruben <ruben.dunesme@etudiant.univ-reims.fr>
2024-12-20 00:45:04 +0100

    feat: adding message when no post found

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,13 @@
 import { Navbar } from "@/components/Navbar";
 import { PostList } from "@/components/post/PostList";
+import { ThemedText } from "@/components/ThemedText";
 import { PostType } from "@/constants/PostType";
-import { deleteAll, getDatas } from "@/functions/fs";
+import { getDatas } from "@/functions/fs";
 import { useThemeColors } from "@/hooks/useThemeColors";
 import { setDatas } from "@/store/reducers/dataSlice";
 import { RootState } from "@/store/store";
-import { useEffect, useState } from "react";
-import { KeyboardAvoidingView, Platform,  StyleSheet } from "react-native";
+import {  useState } from "react";
+import { KeyboardAvoidingView, Platform,  StyleSheet, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useDispatch, useSelector } from "react-redux";
 
@@ -15,29 +16,25 @@ export default function Index() {
   const colors = useThemeColors()
   const datas = useSelector((state: RootState) => state.datas.datas);
   const dispatch = useDispatch();  
-  
-  if(datas.length==0){
-    console.log("Init Datas")
+  const [dataLoaded, setDataLoaded] = useState(false)
+  if(!dataLoaded) {
     getDatas().then((datas : PostType[]) => {
       dispatch(setDatas(datas))
-      console.log('Datas : ',datas)
-  })
-    
-  } 
-  useEffect(()=> {
-
-    console.log(datas)
-
-  },[datas])
+      setDataLoaded(true)
+    })
+  }    
   return (
       <KeyboardAvoidingView
       behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
       style={{ flex: 1 }}
     >
         <SafeAreaView style={[styles.root,{backgroundColor: colors.background}]}>
-          {datas &&
-          <PostList postList={datas}/>
+          <View style={ styles.body}>
+          {datas.length!=0 ?
+          <PostList postList={datas}/>:
+          <ThemedText style={styles.nopost} variant="headline">No news for you :(</ThemedText>
           }
+          </View>
           <Navbar/>          
         </SafeAreaView>
       </KeyboardAvoidingView>
@@ -49,4 +46,14 @@ const styles = StyleSheet.create({
   root: {
     flex:1,
   },
+  body: {
+    flex:1
+  },
+  nopost: {
+    flex:1,
+    textAlign:'center',
+    alignItems:'center',
+    verticalAlign:'middle'
+    
+  }
 })
